a64fa2b5fdaf57ad5911d43264dd176b20b26b39
StefaniaSperani <thedarkcry@hotmail.it>
2022-12-14 15:12:56 +0100

    trait

---
+++ Traits/Seller.php
@@ -0,0 +1,17 @@
+<?php
+
+trait Seller{
+    protected $seller;
+
+    public function getSeller(){
+        return $this->seller;
+    }
+
+    public function setSeller($_seller){
+        return $this->seller
+    }
+}
+
+
+
+?>
--- index.php
+++ index.php
@@ -24,11 +24,10 @@ include __DIR__ . '/Models/Toy.php';
 include __DIR__ . '/Models/Category.php';
 
 $crocchette = new Food('Royal Canin Urinary S/O 1.5kg', 28, './images/gatto.jpg', 'Crocchette per gatto con problemi urinari', 'Pollo', '01/01/2024', new Category('Gatto'));
-//var_dump($crocchette);
+
 $umido = new Food('Royal Canin Urinary S/O 85gr', 1.30, './images/umidogatto.jpg', 'Umido per gatti con problemi urinari', 'Pollo con salsa', '01/01/2024', new Category('Gatto'));
 
 $ossogiocattolo = new Toy('Kong Osso giocattolo con inserimento snacks', 10, './images/cane.jpg', 'Gioco per cani con due fori ai lati per permettere l\'inserimento degli snacks per la pulizia dei denti', new Category('Cane'));
-//var_dump($ossogiocattolo);
 
 $cuccia = new Product('Cuccia morbida', 20, './images/lettocane.jpg', 'Letto per cani in morbida finta pelliccia rivestito esternamente con resistente tessuto, decorato con motivo a zampetta, lavabile in lavatrice, adatto a ogni arredamento.', new Category('Cane'));
 
