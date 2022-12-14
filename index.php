a88a30208de43e3238d3518c08543dc13221e427
StefaniaSperani <thedarkcry@hotmail.it>
2022-12-14 15:33:10 +0100

    trait in index.php

--- index.php
+++ index.php
@@ -23,13 +23,20 @@ include __DIR__ . '/Models/Food.php';
 include __DIR__ . '/Models/Toy.php';
 include __DIR__ . '/Models/Category.php';
 
+
+
 $crocchette = new Food('Royal Canin Urinary S/O 1.5kg', 28, './images/gatto.jpg', 'Crocchette per gatto con problemi urinari', 'Pollo', '01/01/2024', new Category('Gatto'));
+$crocchette->setSeller('Italia');
+//var_dump($crocchette);
 
 $umido = new Food('Royal Canin Urinary S/O 85gr', 1.30, './images/umidogatto.jpg', 'Umido per gatti con problemi urinari', 'Pollo con salsa', '01/01/2024', new Category('Gatto'));
+$umido->setSeller('Italia');
 
 $ossogiocattolo = new Toy('Kong Osso giocattolo con inserimento snacks', 10, './images/cane.jpg', 'Gioco per cani con due fori ai lati per permettere l\'inserimento degli snacks per la pulizia dei denti', new Category('Cane'));
+$ossogiocattolo->setSeller('Italia');
 
 $cuccia = new Product('Cuccia morbida', 20, './images/lettocane.jpg', 'Letto per cani in morbida finta pelliccia rivestito esternamente con resistente tessuto, decorato con motivo a zampetta, lavabile in lavatrice, adatto a ogni arredamento.', new Category('Cane'));
+$cuccia->setSeller('Italia');
 
 $products = [];
 array_push($products, $crocchette, $umido, $ossogiocattolo, $cuccia);
@@ -64,6 +71,10 @@ include __DIR__ . './partials/header.php';
                     <p class="card-text">
                         Categoria:
                         <?php echo $item->category->name; ?>
+                    </p>
+                    <p class="card-text">
+                        Venduto e spedito da:
+                        <?php echo $item->getSeller(); ?>
                     </p>
                 </div>
             </div>
